599fe8dcba7de76e6b00beabafe0a3f82b841902
Khloe Hou <khou@e1z3r2p5.42.us.org>
2019-03-17 23:21:40 -0700

    03/17/2019

--- srcs/hash_file.c
+++ srcs/hash_file.c
@@ -6,7 +6,7 @@
 /*   By: khou <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 12:13:05 by khou              #+#    #+#             */
-/*   Updated: 2019/03/15 13:41:43 by khou             ###   ########.fr       */
+/*   Updated: 2019/03/17 23:16:08 by khou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,31 @@ void	err_obj(char *cmd, char *file)
 	ft_printf("%s: %s: No such file or directory\n", cmd, file);
 }
 
+void	pad_to_hash(t_hash *hash)
+{
+	int	size = 0;
+	
+	hash->len_org = hash->len_org * 8;
+	
+	if (hash->len_org % 512 < 448)
+	{
+		size = 448 - hash->len_org % 512;
+		ft_printf("need to pad: %d\n", size);
+	}
+	else if (hash->len_org % 512 > 448)
+	{
+		size = 512 - hash->len_org % 512 + 448;
+		ft_printf("need to pad: %d\n", size);
+	}
+	
+}
+
+void	prepare_to_hash(t_hash *hash)
+{
+	pad_to_hash(hash);
+}
+
+
 void	hash_file(t_hash * hash, char *cmd, char *file)
 {
 	int		fd;
@@ -24,7 +49,7 @@ void	hash_file(t_hash * hash, char *cmd, char *file)
 	int		ret = 0;
 	char	*tmp;
 
-	ft_printf("BUFF_SIZE: %d\n", BUFF_SIZE);
+//	ft_printf("BUFF_SIZE: %d\n", BUFF_SIZE);
 	if ((fd = open(file, O_RDONLY)) == -1)
 	{
 		err_obj(cmd, file);
@@ -38,16 +63,15 @@ void	hash_file(t_hash * hash, char *cmd, char *file)
 	while ((ret = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
 		buffer[ret] = '\0';
-		printf("buff: %s", buffer);//file has new line
+//		printf("buff: %s", buffer);//file has new line
 		tmp = hash->content;
 		hash->content = ft_strjoin(hash->content, buffer);
 		free(tmp);
 		hash->len_org += ret;
 	}
 	
-	ft_printf("len in char: %d\ncontent: \"\n%s\"", hash->len_org, hash->content);
+	ft_printf("len in char: %d, len in bit: %d\n", hash->len_org, hash->len_org*8);
+//	ft_printf("content: \"\n%s\"\n", hash->content);
 	close(fd);
-	
-
-
+	prepare_to_hash(hash);
 }
